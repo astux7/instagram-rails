c0d7396b52cd6710b2a7a9ddc23efb83b3ad2ca5
astux7 <Asta.bevainyte@gmail.com>
2014-01-25 18:44:52 +0000

    added comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def new
+    @post = Post.find params[:post_id]
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find params[:post_id]
+    @comment = Comment.create(params[:comment].permit(:text))
+    @comment.post_id = @post.id
+    if @comment.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end 
 end
